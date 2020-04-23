2e8ec70bd9a2a988777a076dfb6515cbfa1874f2
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2020-04-23 18:31:53 +0300

    Added an iterative solution

--- factorial.js
+++ factorial.js
@@ -1,5 +1,17 @@
 function factorial(n) {
     return (n != 0) ? n * factorial(--n) : 1; 
 }
-
 console.log(factorial(5));
+
+
+function iterativeFactorial(n) {
+    let result = 1;
+
+    while(n > 0) {
+        result *= n; 
+        n--;
+    }
+
+    return result;
+}
+console.log(iterativeFactorial(5));
