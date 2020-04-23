c5e6d68b9a6fca3b90141f1bdf619b498caabc57
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2020-04-23 18:28:22 +0300

    Refactored to a simpler function

--- factorial.js
+++ factorial.js
@@ -1,13 +1,3 @@
-function calcFactorial(number) {
-    if (number === 0 || number === 1) {
-        return 1;
-    }   
-
-    return number * calcFactorial(number - 1);    
-}
-
-console.log(calcFactorial(5));
-
 function factorial(n) {
     return (n != 0) ? n * factorial(--n) : 1; 
 }
