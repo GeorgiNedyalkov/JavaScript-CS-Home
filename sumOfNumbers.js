5f83b7539584886b9aff0829fe2f7dea628ebfd6
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2020-04-23 15:16:57 +0300

    Added a new exercise

---
+++ sumOfNumbers.js
@@ -0,0 +1,13 @@
+function sumSequenceOfNumbers (numberOne, numberTwo) {
+    let n = Number(numberOne);
+    let m = Number(numberTwo);
+    let sum = 0;
+
+    for (let i = n; i <= m; i++) {
+        sum += i; 
+    }
+
+    return sum;    
+}
+
+console.log(sumSequenceOfNumbers('1', '5'));
