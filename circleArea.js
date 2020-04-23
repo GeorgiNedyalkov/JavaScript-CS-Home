e7f4827cb62e45f5db26988da09bd0aec2b4ef52
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2020-04-23 16:01:04 +0300

    Made a function to calculate circle area

--- circleArea.js
+++ circleArea.js
@@ -5,7 +5,7 @@ function calCircleArea(radius) {
         return 'We can not calculate the circle area, because we receive a string.';
     } else {
         circleArea = Math.PI * (radius ** 2);
-        return circleArea.toFixed(2);
+        return area.toFixed(2);
     }
 }
 console.log(calCircleArea(5));
